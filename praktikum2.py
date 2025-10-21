1ba8f2b9f2b9e0b201208c92f4f45f5949a9f178
Muhammad Rizal Nurfirdaus <M.Rizal.Nurfirdaus@outlook.com>
2025-10-21 11:06:17 +0700

    first commit

---
+++ praktikum2.py
@@ -0,0 +1,4 @@
+# ini adalah komentar
+print("Selamat Belajar Python")
+print("Di Lab Fakultas Ilmu Komputer Universitas Kuningan") # ini juga komentar
+# println("statement ini tidak akan dipanggil")
---
+++ praktikum3.py
@@ -0,0 +1,7 @@
+nama = "Tito Sugiharto"
+hobi = 'Membaca'
+umur = 40
+laki = True
+print("==== biodata ====")
+print("nama: %s" % (nama))
+print("hobi: %s, umur: %d, laki: %r" % (hobi, umur, laki))
